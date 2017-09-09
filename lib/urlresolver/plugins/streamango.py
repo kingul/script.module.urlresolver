e3b2b640b4bb5ba25235eaecc67138314fb8ace4
jsergio123 <12888802+jsergio123@users.noreply.github.com>
2017-09-09 16:10:52 -0400

    Update streamango.py

--- lib/urlresolver/plugins/streamango.py
+++ lib/urlresolver/plugins/streamango.py
@@ -18,7 +18,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import re, js2py
 from lib import helpers
+from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
 
 class StreamangoResolver(UrlResolver):
@@ -26,8 +28,30 @@ class StreamangoResolver(UrlResolver):
     domains = ['streamango.com']
     pattern = '(?://|\.)(streamango\.com)/(?:f/|embed/)?([0-9a-zA-Z]+)'
     
+    def __init__(self):
+        self.net = common.Net()
+    
     def get_media_url(self, host, media_id):
-        return helpers.get_media_url(self.get_url(host, media_id), patterns=['src:"(?P<url>[^"]+)"[^}]+height:(?P<label>\d+)'])
+        web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(web_url, headers=headers).content
+        
+        if html:
+            source = re.search('''srces\.push\({type:"video/mp4",src:\s*((\w+)\(.+?\))''', html)
+            if source:
+                packed = helpers.get_packed_data(html)
+                packed = re.sub('eval\s*\(.*\)', '', packed)
+                js = packed+";"+source.group(1)+";"
+                # I dont like this but we'll see how it goes
+                _source = js2py.eval_js(js.replace("window.%s" % source.group(2), source.group(2)))
+                
+                if _source:
+                    _source = "http:%s" % _source if _source.startswith("//") else _source
+                    headers.update({'Referer': web_url})
+                    return _source + helpers.append_headers(headers)
+        
+        raise ResolverError("Unable to locate video")
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, 'http://{host}/embed/{media_id}')
+
